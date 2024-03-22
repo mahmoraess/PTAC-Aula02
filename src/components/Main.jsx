5a3c6df48c6ed772ee018f1cf46368995a8408c5
mahmoraess <maria.moraes6@estudante.ifms.edu.br>
2024-03-22 17:27:21 -0300

    aula22/03/22

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,26 @@ import { useState } from "react";
 export default function Main(){
     const [nome, setNome] = useState("");
     const [telefone, setTelefone] = useState(0);
+    const [cpf, setCpf] = useState(0);
+    const [listaContatos, setListaContatos] = useState([]);
+
+    const registrar = (event) =>{
+     event.preventDefault(); 
+     alert("salvo");
+     setListaContatos([...listaContatos,
+      {
+        nomeSalvo: nome,
+        telefoneSalvo: telefone,
+        cpfSalvo: cpf,
+      }
+    ]);
+    };
+    console.table(listaContatos)
 
     return(
        <main>
+        <form action="" onSubmit={registrar}>
+          <label htmlFor="nome">Nome:</label>
          <input
          type="text"
          name="" id=""
@@ -20,6 +37,16 @@ export default function Main(){
          onChange={(event)=> setTelefone(event.target.value)}
          />
          {telefone}
+
+         <input
+         type="number"
+         name="" id=""
+         onChange={(event)=> setCpf(event.target.value)}
+         />
+         {cpf}
+
+         <button>Salvar</button>
+         </form>
        </main>
     );
 }
